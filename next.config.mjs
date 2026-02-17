e3514d5b66220e3e6eeb5e478a0e5fa2dd2f243c
shaurya-afk <shauryakumarsharma0007@gmail.com>
2026-02-17 23:48:52 +0530

    updated sitemaps config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
